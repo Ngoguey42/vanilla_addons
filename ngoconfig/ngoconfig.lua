4589ba8901d56f67a78d2fc60f60e6be896d795a
Ngoguey42 <ngoguey@airware.com>
2019-08-09 22:12:48 +0100

    Improve ngoconfig

--- ngoconfig/ngoconfig.lua
+++ ngoconfig/ngoconfig.lua
@@ -58,28 +58,78 @@ function ngoconfig_onevent(event, arg1, arg2, ...)
       1, 2, 3, 4, 5, 6
     }
     local spells = {
-      ["Auto Shoot"] = 26,
-      ["Berserking"] = 66, --TROLL
-      ["Attack"] = 11,
-      ["Raptor Strike"] = 12,
+      ["Berserking"] =          60 + 6, --TROLL
+      ["Attack"] =               0 + 11,
+
+      ["Auto Shot"] =           24 + 2, -- for range 35
+      ["Raptor Strike"] =        0 + 12,
+      ["Track Beasts"] =        24 + 3,
+      ["Aspect of the Monkey"] =60 + 1,
+      ["Serpent Sting"] =        0 + 3,
+      ["Arcane Shot"] =          0 + 2,
+      ["Hunter's Mark"] =        0 + 7,
+      ["Concussive Shot"] =      0 + 4,
+      ["Beast Training"] =      36 + 8,
+      ["Call Pet"] =            60 + 11,
+      ["Dismiss Pet"] =         48 + 8,
+      ["Feed Pet"] =            48 + 3,
+      ["Revive Pet"] =          36 + 9,
+      ["Tame Beast"] =          24 + 8,
+      ["Aspect of the Hawk"] =  60 + 3,
+      ["Track Humanoids"] =     36 + 4,
+      -- ["Distracting Shot"] = ,
+      ["Mend Pet"] =            60 + 9,
+      ["Wing Clip"] =            0 + 10, -- for range 5
+      -- ["Eagle Eye"] = ,
+      -- ["Eyes of the Beast"] = ,
+      ["Scare Beast"] =         24 + 1, -- for range 10
+      -- ["Mongoose Bite"] = ,
+      ["Immolation Trap"] =      0 + 6,
+      -- ["Multi-Shot"] = ,
+      -- ["Track Undead"] = ,
+      ["Aspect of the Cheetah"] =60+ 2,
+      -- ["Disengage"] = ,
+      ["Freezing Trap"] =       48 + 6,
+      -- ["Scorpid Sting"] = ,
+      ["Beast Lore"] =          36 + 2, -- for range 40
+      -- ["Track Hidden"] = ,
+      ["Rapid Fire"] =          60 + 7,
+      -- ["Track Elementals"] = ,
+      -- ["Frost Trap"] = ,
+      -- ["Aspect of the Beast"] = ,
+      ["Feign Death"] =         60 + 10,
+      ["Flare"] =               48 + 7,
+      -- ["Track Demons"] = ,
+      -- ["Explosive Trap"] = ,
+      -- ["Viper Sting"] = ,
+      ["Aspect of the Pack"] = 60 + 2, -- overlap with Cheetah
+      -- ["Track Giants"] = ,
+      -- ["Volley"] = ,
+      -- ["Aspect of the Wild"] = ,
+      -- ["Track Dragonkin"] = ,
+      -- ["Tranquilizing Shot"] = ,
+      ["Intimidation"] =        48 + 2,
+      ["Bestial Wrath"] =       60 + 5,
+
     }
     local macroes = {
-      ["autoshoot"] = 1,
+      ["autoshoot"] = 0 + 1,
       ["/ezq"] = 28,
-      ["/fg"] = 29,
-      ["asspe"] = 58,
-      -- ["togta"] = 59,
       ["relui"] = 40,
-      -- ["togcl"] = 60,
+
+      ["asspe"] = 48 + 10,
+      ["togta"] = 48 + 11,
+      ["togcl"] = 48 + 12,
     }
     local items = {
-      ["Hearthstone"] = 39,
+      ["Hearthstone"] = 36 + 3,
       ["Forest Mushroom Cap"] = 72,
+      ["Cactus Apple Surprise"] = 48 + 4,
+      ["Really Sticky Glue"] = 36 + 1, -- for range 30
     }
 
     local function spellbookIter() --Spellbook iterator
       local i = 1
-      print("spellbookIter??");
 
       return function ()
 	local spellName, _ = GetSpellBookItemName(i, BOOKTYPE_SPELL)
@@ -146,10 +196,10 @@ function ngoconfig_onevent(event, arg1, arg2, ...)
       print("    Placing spells");
       for sId, sName in spellbookIter() do --Place requested spells
         local spellSlot = spells[sName]
-	print(sId, sName, spellSlot);
+	-- print(sId, sName, spellSlot);
 
         if spellSlot ~= nil then
-	  PickupSpell(sId, BOOKTYPE_SPELL)
+	  PickupSpellBookItem(sId, BOOKTYPE_SPELL)
 	  PlaceAction(spellSlot)
 	  ClearCursor()
         end
@@ -198,6 +248,30 @@ function ngoconfig_onevent(event, arg1, arg2, ...)
       -- SetCVar("cameraDistanceMax", 50);
       -- SetCVar("cameraDistanceMaxFactor", 1); -- Not really usefull
       -- SetCVar("cameraDistanceMoveSpeed", 50);
+
+      print("  Updating cvars...");
+
+
+      SetCVar("autoLootDefault", 0);
+      SetCVar("activeCUFProfile", "Primary");
+      SetCVar("cameraSavedDistance", 30.000000);
+      SetCVar("nameplateShowEnemies", 1);
+      SetCVar("nameplateShowEnemyMinions", 1);
+      SetCVar("nameplateShowEnemyPets", 1);
+      SetCVar("nameplateShowEnemyGuardians", 1);
+      SetCVar("nameplateShowEnemyTotems", 1);
+      SetCVar("EJLootClass", 3);
+
+      -- -- Those might not work
+      -- SetCVar("autoQuestPopUps", "v");
+      -- SetCVar("trackedQuests", "v#+O");
+      -- SetCVar("hardTrackedQuests", "v");
+      -- SetCVar("trackedWorldQuests", "v");
+      -- SetCVar("hardTrackedWorldQuests", "v");
+      -- SetCVar("trackedAchievements", "v");
+      -- SetCVar("currencyCategoriesCollapsed", "v");
+      -- SetCVar("minimapShapeshiftTracking", "v");
+      -- SetCVar("reputationsCollapsed", "v##$");
 
       print("  Updating the 4bars content...");
       placeSpells();
