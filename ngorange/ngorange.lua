23fefa1777e5a84706c1f42de1eac68138b8cc25
Ngoguey42 <ngoguey@airware.com>
2019-08-11 18:36:46 +0100

    ngorange 0.2.0

--- ngorange/ngorange.lua
+++ ngorange/ngorange.lua
@@ -1,324 +1,191 @@
-------------------------------------------------
-----------------  CONSTANTES  ----------------
-------------------------------------------------
-local MaxDist = 43
-local IntFact = 100
-local startcolor = {0, 0, 255}
-local endcolor = {255, 0, 0}
-local framewidth = 272
-local BeastString = "Bête"
 
-------------------------------------------------
-----------------  VARIABLES  ----------------
-------------------------------------------------
-local self
-local lu;
+local function setChildrenKeys(self)
+  if self.GetNumChildren == nil then
+    return
+  end
 
-local ngorange_onupdate;
-------------------------------------------------
-----------------  OnLoad    ----------------
-------------------------------------------------
-local function setframesmeta()
-  self.f.bar:SetPoint("CENTER", -86, -254)
-  self.f.bar:SetBackdropColor(0.5, 0.5, 0.5, 0)
-  self.f.bar:SetBackdrop({
+  local f, n, m
+  n = self:GetName().."_"
+
+  for i = 1, self:GetNumChildren(), 1 do
+    f = select(i, self:GetChildren())
+    m = f:GetName()
+    if m ~= nil then
+      m, count = string.gsub(m, n, "")
+      if count == 1 then
+	rawset(self, m, f)
+	setChildrenKeys(f)
+      end
+    end
+  end
+
+  for i = 1, self:GetNumRegions(), 1 do
+    f = select(i, self:GetRegions())
+    m = f:GetName()
+    if m ~= nil then
+      m, count = string.gsub(m, n, "")
+      if count == 1 then
+	rawset(self, m, f)
+	setChildrenKeys(f)
+      end
+    end
+  end
+
+end
+
+local function setupFrames(self)
+
+  self:SetParent(UIParent);
+  self:SetWidth(272);
+  self:SetHeight(28);
+  self:SetFrameStrata("HIGH");
+  self:SetPoint("CENTER", -86, -254)
+  self:SetBackdropColor(1, 0, 0, 0)
+
+  self:SetMovable(true)
+  self:EnableMouse(true)
+  self:RegisterForDrag("LeftButton")
+  self:SetScript("OnDragStart", self.StartMoving)
+  self:SetScript("OnDragStop", self.StopMovingOrSizing)
+
+  self.bar:SetParent(self);
+  self.bar:SetPoint("TOPLEFT", self, "TOPLEFT", 0, 0)
+  self.bar:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", 0, 0)
+  self.bar:SetBackdropColor(0.5, 0.5, 0.5, 0)
+  self.bar:SetBackdrop({
       bgFile = "Interface/Tooltips/UI-Tooltip-Background",
       edgeFile = "Interface/DialogFrame/UI-DialogBox-Border",
       tile = false, tileSize = 16, edgeSize = 15,
       insets = { left = 2, right = 2, top = 2, bottom = 2}
   })
-  self.f.bar:SetWidth(framewidth);
-  self.f.bar:SetHeight(28);
-  self.f.bar:SetAlpha(0.45);
+  self.bar:SetAlpha(0.45);
 
-  self.f.bart:ClearAllPoints();
-  self.f.bart:SetTextColor(1, 1, 1, 0.65)
-  self.f.bart:SetFont("Fonts\\FRIZQT__.TTF", 11)
-  self.f.bart:SetText("0");
-
-  self.f.cursor:SetParent(self.f.bar);
-  self.f.cursor:SetHeight(18);
-  self.f.cursor:SetAlpha(0.65);
-  self.f.cursor:SetBackdrop({
+  self.cursor:SetParent(self);
+  self.cursor:ClearAllPoints()
+  self.cursor:SetPoint("TOPLEFT", self.bar, "TOPLEFT", 0, 8)
+  self.cursor:SetBackdrop({
       bgFile = "Interface/Tooltips/UI-Tooltip-Background",
       edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
       tile = false, tileSize = 16, edgeSize = 15,
-      insets = { left = 3, right =3, top = 3, bottom = 3}
+      insets = {left = 3, right = 3, top = 3, bottom = 3}
   })
-  self.f.cursort:SetPoint("TOP", self.f.cursor, "TOP", 0, 4);
-  self.f.cursort:SetTextColor(1, 1, 1, 0.65)
-  self.f.cursort:SetFont("Fonts\\FRIZQT__.TTF", 11, "OUTLINE")
+  self.cursor:SetHeight(18);
+  self.cursor:SetAlpha(1);
 
-  return ;
-end
-
-local function settextfuncs()
-
+  self.cursor.text:SetPoint("CENTER")
+  self.cursor.text:SetTextColor(1, 1, 1, 0.65)
+  self.cursor.text:SetFont("Fonts\\FRIZQT__.TTF", 11, "OUTLINE")
 
 end
 
-local function setfuncs()
-  function self:UpdateTables()
-    self.minRangeActions = {}
-    self.arrayRangeActions = {}
-    self.BreakPoints = {};
-    if (not UnitExists("target") or UnitIsDead("target")) then
-      self.f.bar:SetAlpha(0);
+
+function moveCursor(self, range)
+  local left, right, middle, width, overflow_left, overflow_right
+
+  local barBorder = 4
+  local totalWidth = self:GetWidth() - barBorder * 2
+
+  left = range.start / NGORANGEMAXRANGE * totalWidth
+  right = range.stop / NGORANGEMAXRANGE * totalWidth
+  middle = (left + right) / 2
+  width = right - left
+
+  -- Apply a minimal width and a bound constraint
+  width = max(33, width)
+  left = middle - width / 2
+  right = middle + width / 2
+  overflow_left = -min(0, left)
+  overflow_right = -min(0, totalWidth - right)
+  left = left + overflow_left - overflow_right
+  right = right - overflow_right + overflow_left
+  middle = (left + right) / 2
+
+  self.cursor:ClearAllPoints()
+  self.cursor:SetPoint("TOP", self, "TOPLEFT", middle + barBorder, -barBorder);
+  self.cursor:SetPoint("BOTTOM", self, "BOTTOMLEFT", middle + barBorder, barBorder);
+  self.cursor:SetWidth(width);
+  self.cursor.text:SetText(range.start.."+");
+end
+
+local function closure()
+  local self
+  local has_target = UnitExists("target")
+  local range_tests
+
+  local function onSlash(msg)
+    if msg == 'reset' then
+      range_tests = ngorangeCreateRangeTests()
+    elseif msg == 'show' then
+      for k, v in ipairs(range_tests) do
+	print('->', v);
+      end
+    else
+      print("| >>> ngorange, a wow classic addon to approximate the target's distance");
+      print('| Works on all targets');
+      print('| The 5 sources of distance:');
+      print('| - Is the spell in range (works with all spells in your spellbook)');
+      print('| - Is the item in range (works with all items in your bags that can be casted on a target)');
+      print('| - Is your target close enough to be followed (28 yards) (works with all units)');
+      print('| - Is your target close enough to be inspected (10 yards) (works with all units)');
+      print('| - Is your target close enough to be healed (40 yards) (works with all units in party/raid)');
+      print('|  ');
+      print('| /ngorange help -> Print this message');
+      print('| /ngorange reset -> Reset the spells and items monitored (useful when new spells or new items)');
+      print("| /ngorange show -> Show what is monitored to approximate the target's distance");
+      print('|  ');
+      print('| Quickly developped by ngo the 11-august-2019 on wow classic 1.13.');
+    end
+  end
+
+  local function onUpdate()
+    local s, r
+    if not has_target then
       return
     end
-    if (not UnitIsFriend("player", "target")) then
-      if (HasAction(10)) then --wing clip /run print(HasAction(68));
-        tinsert(self.minRangeActions, {68, 5})
-      end
-      if (HasAction(37)) then --sticky truc
-        tinsert(self.minRangeActions, {37, 30})
-      end
-      if (UnitCreatureType("target") == BeastString) then
-        if (HasAction(25)) then --beast fear
-          tinsert(self.minRangeActions, {25, 10})
-        end
-        if (HasAction(38)) then --beast lore
-          tinsert(self.minRangeActions, {38, 40})
-        end
-      end
-      if (HasAction(26)) then --autoshoot
-        tinsert(self.arrayRangeActions, {26, 8, 35})
-      end
-      self.f.bar:SetAlpha(1);
-    else
-      self.f.bar:SetAlpha(0.5);
+    assert(range_tests ~= nil)
+    r = ngorangeCreateRange()
+    for k, v in ipairs(range_tests) do
+      s = v()
+      r = r * s
     end
-    for k, v in ipairs (self.interactDistances) do
-      tinsert(self.BreakPoints, {v[2], 1, v[1]});
+    moveCursor(self, r)
+  end
+
+  local function onEvent(_, event)
+    if (event == "PLAYER_ENTERING_WORLD") then
+      ngorange:UnregisterEvent("PLAYER_ENTERING_WORLD")
+      ngorange:SetScript("OnUpdate", onUpdate);
+      range_tests = ngorangeCreateRangeTests()
     end
-    for k, v in ipairs (self.minRangeActions) do
-      tinsert(self.BreakPoints, {v[2], 2, v[1]});
-    end
-    for k, v in ipairs (self.arrayRangeActions) do
-      tinsert(self.BreakPoints, {v[2], 3, v[1]});
-      tinsert(self.BreakPoints, {v[3], 4, v[1]});
-    end
-    sort(self.BreakPoints, function (a, b) return a[1] < b[1];end)
-    for k, v in ipairs (self.BreakPoints) do
-      local str;
-      if (v[2] == 2) then
-        str = tostring(GetActionTexture(v[3]));
-      else
-        str = "";
-      end
-    end
-    for k, v in ipairs (self.BreakPoints) do
+    if (event == "PLAYER_TARGET_CHANGED") then
+      has_target = UnitExists("target")
     end
   end
 
-  function self:DetermineRangeArray()
-    local maxpossible = MaxDist;
-    local minpossible = 0;
-    local test;
-    for k, v in ipairs (self.BreakPoints) do
-      if (v[2] <= 2) then
-        if (v[2] == 1) then
-          test = CheckInteractDistance("target", v[3]);
-        elseif (v[2] == 2) then
-          test = IsActionInRange(v[3]);
-        end
-        if (test) then
-          maxpossible = v[1];
-          break ;
-        else
-          minpossible = v[1];
-        end
-      end
-    end
-    for k, v in ipairs (self.BreakPoints) do
-      if (v[2] >= 3 and minpossible < v[1] and maxpossible > v[1]) then
-        test = IsActionInRange(v[3]);
-        if (v[2] == 3) then
-          if (test) then
-            minpossible = v[1];
-          else
-            maxpossible = v[1];
-          end
-        elseif (v[2] == 4) then
-          if (test) then
-            maxpossible = v[1];
-          else
-            minpossible = v[1];
-          end
-        end
-      end
-    end
-    if (self.minv ~= minpossible or self.maxv ~= maxpossible) then
-      self.minv = minpossible;
-      self.maxv = maxpossible;
-      return (1);
-    end
-    return (0);
+  local function onLoad()
+    self = ngorange
+    self:SetScript("OnEvent", onEvent);
+    self:RegisterEvent("PLAYER_TARGET_CHANGED")
+    self:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+    setChildrenKeys(self)
+    setupFrames(self)
+    print("Welcome to ngorange. Commands: `/ngorange help`, `/ngorange show`, `/ngorange reset`.");
   end
 
-  function self.f.bar.setpane(pane, minv, maxv, leftanchor)
-    local percent;
-    local color;
-
-    pane.minv = minv;
-    pane.maxv = maxv;
-    percent = (maxv - minv) / MaxDist;
-    pane:SetWidth(percent * (framewidth - 6));
-    percent = (maxv / 2 + minv / 2) / MaxDist;
-    color = {};
-    for i = 1, 3, 1 do
-      color[i] = (startcolor[i] + percent * (endcolor[i] - startcolor[i])) / 255;
-    end
-    color[4] = 1;
-    pane:SetBackdropColor(unpack(color))
-    if (leftanchor == nil) then
-      pane:SetPoint("LEFT", self.f.bar, "LEFT", 3, 0);
-    else
-      pane:SetPoint("LEFT", leftanchor, "RIGHT");
-    end
-    pane.t:SetText(maxv);
-    pane:Show()
-  end
-
-  function self.f.bar.updateBackground()
-    local pane;
-    local minv, maxv = 0, 0;
-    local percent;
-    local color;
-    local leftanchor = nil
-
-    for k, v in ipairs (self.BreakPoints) do
-      maxv = v[1];
-      pane = self.f.bg[k];
-      self.f.bar.setpane(pane, minv, maxv, leftanchor);
-      leftanchor = pane;
-      minv = maxv;
-    end
-    self.f.bar.setpane(self.f.bg[getn(self.BreakPoints) + 1], minv, MaxDist, leftanchor);
-    for k = getn(self.BreakPoints) + 2, getn(self.f.bg), 1 do
-      pane = self.f.bg[k];
-      pane:Hide()
-    end
-    if (rawget(self.f.bg, 1)) then
-      self.f.bart:SetPoint("TOP", self.f.bg[1], "BOTTOMLEFT", 0, 11);
-    else
-      self.f.bart:ClearAllPoints();
-    end
-  end
-
-  function self:SetCursor()
-    for k, v in ipairs (self.f.bg) do
-      if (not v:IsShown(v)) then
-        break ;
-      end
-      if (v.minv == self.minv) then
-        local lap = 33 - v:GetWidth();
-        if (lap > 0) then
-          self.f.cursor:SetPoint("BOTTOMLEFT", v, "TOPLEFT", - lap / 2, -12);
-          self.f.cursor:SetPoint("BOTTOMRIGHT", v, "TOPRIGHT",  lap / 2, -12);
-        else
-          self.f.cursor:SetPoint("BOTTOMLEFT", v, "TOPLEFT", 0, -12);
-          self.f.cursor:SetPoint("BOTTOMRIGHT", v, "TOPRIGHT", 0, -12);
-        end
-        self.f.cursor:SetFrameStrata("MEDIUM");
-        self.f.cursort:SetText(self.minv.."+");
-        break ;
-      end
-    end
-  end
-  return ;
+  return onLoad, onSlash
 end
 
-function ngorange_onload()
-  self = ngorange;
-
-  if (self == nil) then
-    print("ngorange frame not found ! :'(");
-  end
-  self.min = 0;
-  self.max = MaxDist;
-  self.minRangeActions = {};
-  self.arrayRangeActions = {};
-  self.interactDistances = {};
-  self.BreakPoints = {};
-  tinsert(self.interactDistances, {4, 28});
-  self:SetPoint("CENTER", 0, 0);
-  self.f.bg = {};
-
-  setmetatable(self.f.bg, {__index = function(t, k)
-			     local parent = self.f.bar;
-			     local pane = CreateFrame("Frame", (parent:GetName().."Pane"..k), parent, "ngorangevirtual")
-			     local text = getfenv()[pane:GetName().."t"];
-
-			     pane:SetHeight(parent:GetHeight());
-			     pane:SetBackdrop( { bgFile = "Interface/Tooltips/UI-Tooltip-Background",
-						 tile = false, tileSize = 16, edgeSize = 0,
-						 insets = { left = 0, right = 0, top = 3, bottom = 3}
-			     })
-			     pane.t = text;
-			     text:ClearAllPoints();
-			     text:SetPoint("TOP", pane, "BOTTOMRIGHT", 0, 11);
-			     text:SetTextColor(1, 1, 1, 0.65)
-			     text:SetFont("Fonts\\FRIZQT__.TTF", 11)
-			     text:SetText("0");
-			     rawset(t, k, pane)
-			     return pane
-  end})
-
-  setframesmeta()
-  settextfuncs()
-  setfuncs()
-
-  self:RegisterEvent("PLAYER_TARGET_CHANGED")
-  self:RegisterEvent("PLAYER_ENTERING_WORLD")
-end
-
-------------------------------------------------
-----------------  OnEvent    ----------------
-------------------------------------------------
-function ngorange_onevent(event, arg1, arg2, ...)
-
-  if (event == "PLAYER_ENTERING_WORLD") then
-    self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-    lu = GetTime()
-    self:SetScript("OnUpdate", ngorange_onupdate);
-    self:UpdateTables()
-    -- for k, v in pairs (self.f.bg) do
-    -- print(tostring(k).."("..tostring(v)..")");
-    -- end
-  end
-
-  if (event == "PLAYER_TARGET_CHANGED") then
-    self:UpdateTables()
-    self.f.bar.updateBackground()
-  end
-end
-------------------------------------------------
--------------  Fonctions Générales  ------------
-------------------------------------------------
-
-function ngorange_setDoubleParentKey(child, parent)
-  local pname, cname, key;
-
-  if (parent.f == nil) then
-    parent.f = {};
-  end
-  pname = parent:GetName();
-  cname = child:GetName();
-  key = string.sub(cname, string.len(pname) + 1);
-  parent.f[key] = child;
-  key = key.."t"
-  parent.f[key] = getfenv()[pname..key];
-end
-
-------------------------------------------------
-----------------  OnUpdate  ----------------
-------------------------------------------------
-function ngorange_onupdate()
-  local elapsed = GetTime() - lu;
-
-  lu = GetTime();
-  if (self:DetermineRangeArray()) then
-    self:SetCursor()
+ngorange_onload, ngorange_onslash = closure()
+SLASH_NGORANGE1 = "/ngorange"
+SlashCmdList["NGORANGE"] = ngorange_onslash
+function LOL()
+  range_tests = ngorangeCreateRangeTests()
+  local s, r
+  r = ngorangeCreateRange()
+  for k, v in ipairs(range_tests) do
+    s = v()
+    r = r * s
+    print(v, s, r);
   end
 end
---
+++ ngorange/ranges.lua
@@ -0,0 +1,222 @@
+local MAXRANGE = 43
+
+local function tribool(v)
+  if v == nil then
+    return nil
+  else
+    if v == 1 or v == true then
+      return true
+    elseif v == 0 or v == false then
+      return false
+    else
+      assert(false)
+    end
+  end
+end
+
+local function createRange(start, stop)
+  local o = {}
+
+  local function str(r)
+    return '('..tostring(r.start)..', '..tostring(r.stop)..')'
+  end
+
+  local function mul(r0, r1)
+    local start = max(r0.start, r1.start)
+    local stop = min(r0.stop, r1.stop)
+
+    if start > stop then
+      -- /!\ I duknow if it happens, let's hide this naughty error!!
+      -- print('Faulty mul on', r0, r1);
+      start = stop
+    end
+    return createRange(start, stop)
+  end
+
+  o.start = start or 0
+  o.stop = stop or MAXRANGE
+  setmetatable(o, {__mul=mul, __tostring=str})
+  return o
+end
+
+local function createSpell(spellIdOrName)
+  local o = {}
+  local name, _, _, _, minrange, maxrange, id, _ = GetSpellInfo(spellIdOrName)
+
+  o.true_range = createRange(minrange, maxrange)
+  local function call()
+    local inrange = tribool(IsSpellInRange(name, "target")) -- /!\ does not use id!
+    local possible = inrange ~= nil and UnitExists("target") and not UnitIsDeadOrGhost("target")
+
+    if possible and inrange then
+      return createRange(minrange, maxrange)
+    elseif possible and minrange == 0 then
+      return createRange(maxrange, MAXRANGE)
+    else
+      return createRange()
+    end
+  end
+  local function str()
+    return name..':'..id..'@'..minrange..'-'..maxrange..'y'
+  end
+
+  setmetatable(o, {__call=call, __tostring=str})
+  return o
+end
+
+local function createItem(itemIdOrName)
+  local o = {}
+
+  local name = GetItemInfo(itemIdOrName)
+  local sname, sid = GetItemSpell(itemIdOrName)
+  assert(sname ~= nil)
+  local _, _, _, _, minrange, maxrange, _, _ = GetSpellInfo(sid)
+  assert(maxrange ~= nil)
+
+  o.true_range = createRange(minrange, maxrange)
+  local function call()
+    local inrange = tribool(IsItemInRange(itemIdOrName, "target"))
+    local possible = inrange ~= nil and UnitExists("target") and not UnitIsDeadOrGhost("target")
+
+    if possible and inrange then
+      return createRange(minrange, maxrange)
+    elseif possible and minrange == 0 then
+      return createRange(maxrange, MAXRANGE)
+    else
+      return createRange()
+    end
+  end
+  local function str()
+    local s = name..':'..itemIdOrName..'('..sname..':'..sid..')@'..minrange..'-'..maxrange..'y'
+    return s
+  end
+
+  setmetatable(o, {__call=call, __tostring=str})
+  return o
+end
+
+local function createInteractDistance(idx)
+  local o = {}
+  local names = {
+    [1] = "Inspect",
+    [2] = "Trade",
+    [3] = "Duel",
+    [4] = "Follow",
+  }
+  local maxranges = {
+    [1] = 10, -- 28y starting from wow version ~3.0
+    [2] = 11,
+    [3] = 10,
+    [4] = 28,
+  }
+
+  o.true_range = createRange(0, maxranges[idx])
+  local function call()
+    local test = CheckInteractDistance("target", idx)
+    if test == 1 or test == true then
+      return createRange(0, maxranges[idx])
+    elseif UnitExists("target") then
+      return createRange(maxranges[idx], MAXRANGE)
+    else
+      return createRange()
+    end
+  end
+  local function str()
+    return names[idx]..'@'..'0-'..maxranges[idx]..'y'
+  end
+
+  setmetatable(o, {__call=call, __tostring=str})
+  return o
+end
+
+local function createUnitInRange()
+  local o = {}
+
+  o.true_range = createRange(0, 40)
+  local function call()
+    local test = UnitInRange("target")
+    if UnitInParty("target") or UnitInRaid("target") ~= nil then
+      if UnitInRange("target") then
+	return createRange(0, 40)
+      else
+	return createRange(40, MAXRANGE)
+      end
+    else
+      return createRange()
+    end
+  end
+  local function str()
+    return 'unitInRange@0-40y'
+  end
+
+  setmetatable(o, {__call=call, __tostring=str})
+  return o
+end
+
+local function spellbookIter()
+  local i = 1
+
+  return function ()
+    local name, subtext = GetSpellBookItemName(i, BOOKTYPE_SPELL)
+
+    if name == nil then
+      return nil
+    end
+    i = i + 1
+    return i - 1, name, subtext
+  end
+end
+
+local function inventoryIter()
+  local bagId = 0
+  local bagSize = GetContainerNumSlots(bagId) or 0
+  local slotId = 1
+  local id, link, name
+
+  return function ()
+    id = nil
+    repeat
+      -- Advance cursor to next item or `return nil`
+      if bagId > 4 then
+	return nil
+      elseif slotId <= bagSize then
+	_, _, _, _, _, _, link, _, _, id = GetContainerItemInfo(bagId, slotId)
+	slotId = slotId + 1
+      else
+	bagId = bagId + 1
+	bagSize = GetContainerNumSlots(bagId) or 0
+	slotId = 1
+      end
+    until id ~= nil
+    name = string.gsub(link, "(.+)%[(.-)%](.+)", "%2")
+    return bagId, slotId - 1, name, id
+  end
+end
+
+local function createRangeTests()
+  local tests = {
+    createInteractDistance(4),
+    createInteractDistance(1),
+    createUnitInRange(),
+  }
+
+  for _, name, subtext in spellbookIter() do
+    local _, _, _, _, minRange, maxRange, spellId = GetSpellInfo(name, subtext)
+    if minRange ~= nil and maxRange ~= nil and minRange < maxRange then
+      table.insert(tests, createSpell(spellId))
+    end
+  end
+  for bagId, slotId, name, id in inventoryIter() do
+    local sname, sid = GetItemSpell(id)
+    if sname then
+      local _, _, _, _, minRange, maxRange, _ = GetSpellInfo(sid)
+      if minRange ~= nil and maxRange ~= nil and minRange < maxRange then
+  	table.insert(tests, createItem(id))
+      end
+    end
+  end
+  return tests
+end
+ngorangeCreateRangeTests = createRangeTests
+ngorangeCreateRange = createRange
+NGORANGEMAXRANGE = MAXRANGE
